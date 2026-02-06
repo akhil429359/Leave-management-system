712919d9c43bd542590c8b6c6e2a0a533f3d4c65
AKHIL PIOUS <akhilpioussince2001@gmail.com>
2026-02-06 13:07:57 +0530

    Added token based auth and login API

--- lms/leaves/views.py
+++ lms/leaves/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 # Create your views here.
+
+class LoginView(APIView):
+    permission_classes = []
+    def post(self,request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username,password=password)
+
+        if  not user:
+            return Response({'error':'Invalid Credentials'},status=status.HTTP_401_UNAUTHORIZED)
+        
+        try:
+            token = Token.objects.get(user=user)
+        except Token.DoesNotExist:
+            token = Token.objects.create(user=user)
+
+        return Response({'token':token.key,'username':user.username,'is_staff':user.is_staff})
